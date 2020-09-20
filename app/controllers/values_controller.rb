a80e92a406ef192afb6cd4ebf908ae222a4c9430
takakha <66158185+takakha@users.noreply.github.com>
2020-09-20 17:32:38 +0900

    価値観の作成ページに遷移

--- app/controllers/values_controller.rb
+++ app/controllers/values_controller.rb
@@ -1,5 +1,10 @@
 class ValuesController < ApplicationController
   def index
     @situation = Situation.find(params[:situation_id])
+    @valus = Value.all
+  end
+
+  def new
+    @situation = Situation.find(params[:situation_id])
   end
 end
