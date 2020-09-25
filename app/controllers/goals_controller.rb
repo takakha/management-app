66619e90536d6123260ebc7282733c18bf7bd36b
takakha <66158185+takakha@users.noreply.github.com>
2020-09-25 15:16:14 +0900

    目標の作成機能

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,6 +1,22 @@
 class GoalsController < ApplicationController
+  
   def index
     @situation = Situation.find(params[:situation_id])
-
+    @goal = Goal.new
   end
+
+  def create  
+    @situation = Situation.find(params[:situation_id])
+    @goal = @situation.goals.new(goal_params)
+    if @goal.save
+    redirect_to situation_goals_path(@situation.id)
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:progress,:startline,:deadline,:name, :situation_id)
+  end
+
 end
