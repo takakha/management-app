9fe3ae9ba4c24651a02a81161175c97d948e22da
takakha <66158185+takakha@users.noreply.github.com>
2020-09-26 09:18:37 +0900

    目標の削除機能

--- app/controllers/purposes_controller.rb
+++ app/controllers/purposes_controller.rb
@@ -12,6 +12,14 @@ class PurposesController < ApplicationController
     redirect_to situation_purposes_path(@situation.id)
     end
   end
+  
+  def destroy
+    @situation = Situation.find(params[:situation_id])
+    @purpose = Purpose.find(params[:id])
+    if @purpose.destroy
+      redirect_to situation_purposes_path(@situation.id)
+    end
+  end
 
   private
 
